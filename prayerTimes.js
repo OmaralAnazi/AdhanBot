7539f67847c1bf5174b644a04dcfd028ec1b251a
Omar Alanazi <oalanazi@villacapital.sa>
2024-05-31 19:59:08 +0300

    fix times

--- prayerTimes.js
+++ prayerTimes.js
@@ -2,7 +2,7 @@ async function getPrayerTimes() {
     const fetch = (await import('node-fetch')).default;
     const city = 'Riyadh';
     const country = 'Saudi Arabia';
-    const method = 2; 
+    const method = 1; 
 
     const response = await fetch(`http://api.aladhan.com/v1/timingsByCity?city=${city}&country=${country}&method=${method}`);
     const data = await response.json();
